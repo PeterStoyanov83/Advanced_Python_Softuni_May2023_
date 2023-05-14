31cd9975bdbf3f84a00c185e6e6c777712d74650
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-05-14 12:17:31 +0200

    .

--- 01_Lists_as_stacks_queues/Exersises /02_stacked_queues.py
+++ 01_Lists_as_stacks_queues/Exersises /02_stacked_queues.py
@@ -2,23 +2,24 @@ from collections import deque
 
 numbers = deque()  # Create an empty deque to store the numbers
 
-# Process the input commands
+map_functions = {
+    1: lambda x: numbers.append(x[1]),  # x == number_data => [1, 97]
+    2: lambda x: numbers.pop() if numbers else None,
+    3: lambda x: print(max(numbers)) if numbers else None,
+    4: lambda x: print(min(numbers)) if numbers else None,
+}
+
 for _ in range(int(input())):
     number_data = [int(x) for x in input().split()]  # Get the command and number data
+
     command = number_data[0]  # Extract the command
 
-    # Process different commands
-    if command == 1:
-        numbers.append(number_data[1])  # Command 1: Add the number to the deque
-    elif command == 2:
-        if numbers:
-            numbers.pop()  # Command 2: Remove the last number from the deque if it's not empty
-    elif command == 3:
-        if numbers:
-            print(max(numbers))  # Command 3: Print the maximum number in the deque if it's not empty
-    elif command == 4:
-        if numbers:
-            print(min(numbers))  # Command 4: Print the minimum number in the deque if it's not empty
+    if command in map_functions:
+        # If the command is valid and present in the map_functions dictionary, execute the corresponding function
+        map_functions[command](number_data)
+    else:
+        # If the command is not valid, print an error message
+        print("Invalid command:", command)
 
 numbers_reversed = reversed(numbers)  # Reverse the deque to print the numbers in reverse order
 print(*numbers_reversed, sep=', ')  # Print the reversed numbers separated by commas
