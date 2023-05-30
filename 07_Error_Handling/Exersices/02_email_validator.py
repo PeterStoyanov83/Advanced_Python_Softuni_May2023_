5cd7c66c944e3045d294262d29fe09d1e2457616
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-05-30 08:58:28 +0200

    .

--- 07_Error_Handling/Exersices/02_email_validator.py
+++ 07_Error_Handling/Exersices/02_email_validator.py
@@ -10,24 +10,27 @@ class InvalidDomainError(ValueError):
     """Exception raised when the email has an invalid domain."""
 
 
-VALID_DOMAINS = ['.com', '.bg', '.net', '.org']
+valid_domains = ['.com', '.bg', '.net', '.org']
 
 while True:
-    email = input()
-    if email == "End":
-        break
+    try:
+        email = input()
+        if email == "End":
+            break
 
-    if "@" not in email:
-        raise MustContainAtSymbolError("Email must contain @")
+        if "@" not in email:
+            raise MustContainAtSymbolError("Email must contain @")
 
-    email_parts = email.split("@")
-    name = email_parts[0]
-    domain = "." + email_parts[1].split(".")[-1]
+        email_parts = email.split("@")
+        name = email_parts[0]
+        domain = "." + email_parts[1].split(".")[-1]
 
-    if len(name) <= 4:
-        raise NameTooShortError("Name must be more than 4 characters")
+        if len(name) <= 4:
+            raise NameTooShortError("Name must be more than 4 characters")
 
-    if domain not in VALID_DOMAINS:
-        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+        if domain not in valid_domains:
+            raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
 
-    print("Email is valid")
+        print("Email is valid")
+    except (NameTooShortError, MustContainAtSymbolError, InvalidDomainError) as e:
+        print(e)
