3982df47c40c1e14abd843e57da886c40fe81f7b
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-05-31 13:11:07 +0200

    .

--- 08_File_handling/exersices/04_directory_traversal.py
+++ 08_File_handling/exersices/04_directory_traversal.py
@@ -3,17 +3,15 @@ import collections
 
 
 def traverse_directory(path):
-    # Initialize a default dictionary with a list as the default value type
     files_by_extension = collections.defaultdict(list)
 
     for entry in os.scandir(path):
         if entry.is_file():
             filename, file_extension = os.path.splitext(entry.name)
-            # strip the leading '.' from the extension and convert to lower case
+
             file_extension = file_extension[1:].lower()
             files_by_extension[file_extension].append(filename)
 
-    # sort extensions and filenames
     for file_extension in files_by_extension:
         files_by_extension[file_extension].sort()
     sorted_extensions = collections.OrderedDict(sorted(files_by_extension.items()))
