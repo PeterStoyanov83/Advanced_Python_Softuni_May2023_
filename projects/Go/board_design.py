22df8f16cecc18ae86882e7d850e10a54da03ff1
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-02 23:09:14 +0200

    .

--- projects/Go/board_design.py
+++ projects/Go/board_design.py
@@ -2,41 +2,22 @@ import pygame
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
-GRID_SIZE = 30
-MARGIN = 2
+BROWN = (139, 69, 19)
 
 
 class GameBoard:
     def __init__(self, screen, board_size):
         self.screen = screen
         self.board_size = board_size
-        self.board_color = WHITE
-        self.grid_color = BLACK
-        self.grid_width = 1
-        self.stone_radius = int(GRID_SIZE / 2) - 2
+        self.grid_size = screen.get_width() // (board_size + 2)  # Calculate grid size based on screen width
 
     def draw_board(self, stones):
-        self.screen.fill(self.board_color)
+        self.screen.fill(BROWN)
 
         for row in range(self.board_size):
             for col in range(self.board_size):
-                x = MARGIN + col * GRID_SIZE
-                y = MARGIN + row * GRID_SIZE
-                pygame.draw.circle(self.screen, self.grid_color, (x, y), self.stone_radius, self.grid_width)
-
-        for row in range(self.board_size):
-            for col in range(self.board_size):
-                x = MARGIN + col * GRID_SIZE
-                y = MARGIN + row * GRID_SIZE
-                stone = stones[col][row]
-                if stone == BLACK:
-                    stone_color = (0, 0, 0)  # Black color
-                elif stone == WHITE:
-                    stone_color = (255, 255, 255)  # White color
-                else:
-                    stone_color = (0, 0, 0)  # Default color for empty spaces
-                if stone is not None:
-                    pygame.draw.circle(self.screen, stone_color, (x, y), self.stone_radius)
+                x = (col + 1) * self.grid_size
+                y = (row + 1) * self.grid_size
+                pygame.draw.rect(self.screen, BLACK, (x, y, self.grid_size, self.grid_size), 1)
 
         pygame.display.flip()
--- projects/Go/welcome_screen.py
+++ projects/Go/welcome_screen.py
@@ -1,60 +1,84 @@
 import pygame
-from board_design import GameBoard
-from game_over_screen import game_over_screen
-
-# Constants for the game
-BOARD_SIZE = 19
-GRID_SIZE = 30
-SCREEN_SIZE = (BOARD_SIZE * GRID_SIZE, BOARD_SIZE * GRID_SIZE)
-PLAYER_VS_PLAYER = 1
-PLAYER_VS_COMPUTER = 2
 
 
-def welcome_screen(screen, SCREEN_SIZE):
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    screen.fill(WHITE)  # Fill the screen with white color
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BROWN = (139, 69, 19)
 
-    font = pygame.font.Font(None, 36)
-    title_text = font.render("Go Game", True, BLACK)
-    screen.blit(title_text, (SCREEN_SIZE[0] // 2 - title_text.get_width() // 2, 50))
 
-    pvp_text = font.render("Player vs Player", True, BLACK)
-    screen.blit(pvp_text, (SCREEN_SIZE[0] // 2 - pvp_text.get_width() // 2, 150))
+class GameBoard:
+    def __init__(self, screen, board_size):
+        self.screen = screen
+        self.board_size = board_size
+        self.grid_size = screen.get_width() // (board_size + 2)
 
-    pvc_text = font.render("Player vs Computer", True, BLACK)
-    screen.blit(pvc_text, (SCREEN_SIZE[0] // 2 - pvc_text.get_width() // 2, 200))
+    def draw_board(self, stones):
+        self.screen.fill(BROWN)
 
-    pygame.display.flip()  # Update the display
+        for row in range(self.board_size):
+            for col in range(self.board_size):
+                x = (col + 1) * self.grid_size
+                y = (row + 1) * self.grid_size
+                pygame.draw.rect(self.screen, BLACK, (x, y, self.grid_size, self.grid_size), 1)
 
-    # Wait for player's selection
-    while True:
+        pygame.display.flip()
+
+
+def play_game():
+    # Constants for the game
+    BOARD_SIZE = 19
+    GRID_SIZE = 30
+    SCREEN_SIZE = (BOARD_SIZE * GRID_SIZE, BOARD_SIZE * GRID_SIZE)
+
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("Go Game")
+
+    game_board = GameBoard(screen, BOARD_SIZE)
+
+    stones = [["" for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+    current_player = "B"
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if pvp_text.get_rect(topleft=(SCREEN_SIZE[0] // 2 - pvp_text.get_width() // 2, 150)).collidepoint(
-                        mouse_pos
-                ):
-                    return PLAYER_VS_PLAYER, 0
-                elif pvc_text.get_rect(topleft=(SCREEN_SIZE[0] // 2 - pvc_text.get_width() // 2, 200)).collidepoint(
-                        mouse_pos
-                ):
-                    # Check the intelligence level selection
-                    if level1_text.get_rect(
-                            topleft=(SCREEN_SIZE[0] // 2 - level1_text.get_width() // 2 - 100, 450)).collidepoint(
-                            mouse_pos
-                    ):
-                        return PLAYER_VS_COMPUTER, 1
-                    elif level2_text.get_rect(
-                            topleft=(SCREEN_SIZE[0] // 2 - level2_text.get_width() // 2, 450)).collidepoint(
-                            mouse_pos
-                    ):
-                        return PLAYER_VS_COMPUTER, 2
-                    elif level3_text.get_rect(
-                            topleft=(SCREEN_SIZE[0] // 2 - level3_text.get_width() // 2 + 100, 450)).collidepoint(
-                            mouse_pos
-                    ):
-                        return PLAYER_VS_COMPUTER, 3
+                running = False
+
+        game_board.draw_board(stones)
+
+        # Handle player move
+        mouse_pos = pygame.mouse.get_pos()
+        x = mouse_pos[0] // GRID_SIZE
+        y = mouse_pos[1] // GRID_SIZE
+
+        if (
+            x >= 0
+            and x < BOARD_SIZE
+            and y >= 0
+            and y < BOARD_SIZE
+            and stones[x][y] == ""
+        ):
+            stones[x][y] = current_player
+            current_player = "W" if current_player == "B" else "B"
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+def main():
+    pygame.init()
+
+    SCREEN_SIZE = (400, 300)
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("Go Game")
+
+    play_game()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
