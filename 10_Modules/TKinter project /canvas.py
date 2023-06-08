c3bfa1c56306a6a9d915074ccf30b7ce14897be7
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-08 13:27:34 +0200

    .

--- 10_Modules/TKinter project /canvas.py
+++ 10_Modules/TKinter project /canvas.py
@@ -1,13 +1,23 @@
-from tkinter import Tk
+from tkinter import Tk, Canvas
 
 
 def create_root():
     root = Tk()
-    root.title("GUI SHOP")
-    root.geometry("700x600")
+
+    root.title("GUI Shop")
+    root.geometry(f"700x600")
     root.resizable(False, False)
 
-    root.mainloop()
+    return root
+
+
+def create_frame():
+    frame = Canvas(root, width=700, height=700)
+    frame.grid(row=0, column=0)
+
+    return frame
 
 
 root = create_root()
+
+frame = create_frame()
