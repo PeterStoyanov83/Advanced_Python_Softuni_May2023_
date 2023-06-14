cc4d13b4523b0255ff4745554fb75006c0ea8032
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-14 12:27:33 +0200

    .

--- 01_Lists_as_stacks_queues/02_matching_parentheses.py
+++ 01_Lists_as_stacks_queues/02_matching_parentheses.py
@@ -1,12 +1,17 @@
-stack_parent = []
+stack_parent = []  # create the stack for the parantecies finder
 
-text = input()
+text = input() #the text we take from the console by user
 
-for index in range(len(text)):
-    if text[index] == '(':
-        stack_parent.append(index)
-        "filinng up the stack until the next parenteses"
-    elif text[index] == ')':
-        start_position = stack_parent.pop()
-        end_position = index + 1
-    print(text[start_position:end_position])
+for index in range(len(text)):              #create the for loop to get through everu index
+    if text[index] == '(':                  #check if index is a (
+        stack_parent.append(index)          #then put it in the stack variable
+
+#this cycle will fill up the stack with the text between the ()
+
+    elif text[index] == ')':                    #check for the closing )
+        start_position = stack_parent.pop()     #if the inxex is ) remove it from the stack and define the index in text
+                                                #as start position
+        end_position = index + 1                #define the next index as the ending of the end position
+    print(text[start_position:end_position])    #print the text that was between the two intexes with "()"
+
+
